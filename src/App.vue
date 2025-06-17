d75f33d132fd66e2796d48a252e2acc405b31cce
Ayat166 <ayatali1661@gmail.com>
2025-06-17 18:36:03 +0300

    add footer layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Navbar from './components/Navbar.vue'
-
+import Footer from './components/Footer.vue'
 export default defineComponent({
     name: 'App',
     components: {
-        Navbar
+        Navbar,
+        Footer
     }
 })
 </script>
@@ -13,6 +14,7 @@ export default defineComponent({
 <template>
     <Navbar />
     <router-view />
+    <Footer />
 </template>
 
 <style scoped>
