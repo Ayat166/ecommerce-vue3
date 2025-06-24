ff6af37ecb2580214eed4e185e8af96f98cc4d8b
Ayat166 <ayatali1661@gmail.com>
2025-06-24 20:00:17 +0300

    Add TypeScript declaration for Vue file imports to define type logic

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,32 +1,46 @@
 import { createStore } from 'vuex';
 
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  image: string;
+}
+export interface State {
+  products: Product[];
+  product: Product | null;
+}
+export interface Commit{
+  (type: string, payload?: any): void;
+}
 export default createStore({
   state: {
-    products: [],
+    products: [] as Product[],
     product: null
   },
   mutations: {
-    setProducts(state, products) {
+    setProducts(state: State, products: Product[]) {
       state.products = products;
     },
-    setProduct(state, product) {
+    setProduct(state: State, product: Product | null) {
       state.product = product;
     } 
   },
   actions: {
-    async fetchProducts({ commit }) {
+    async fetchProducts({ commit }: { commit: Commit  }) {
       const res = await fetch('https://fakestoreapi.com/products');
       const data = await res.json();
       commit('setProducts', data);
     },
-    async fetchProduct({ commit }, id) {
+    async fetchProduct({ commit }: { commit: Commit }, id: number) {
       const res = await fetch(`https://fakestoreapi.com/products/${id}`);
       const data = await res.json();
       commit('setProduct', data);
     }
   },
   getters: {
-    allProducts: (state) => state.products,
-    currentProduct: (state) => state.product
+    allProducts: (state: State) => state.products,
+    currentProduct: (state: State) => state.product
   }
 });
