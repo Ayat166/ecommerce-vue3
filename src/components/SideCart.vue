f7f16bb342ee9e6c9c774a8b381042b73f1591a8
Ayat166 <ayatali1661@gmail.com>
2025-07-03 10:02:19 +0300

    migrate side cart component to composition

--- src/components/SideCart.vue
+++ src/components/SideCart.vue
@@ -40,34 +40,50 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 import { RouterLink } from 'vue-router';
-import { mapGetters, mapActions } from 'vuex';
-export default defineComponent({
-  props: {
-    isActive: {
-      type: Boolean,
-      required: true
-    }
-  },
-  components: {
-    RouterLink
-  },
-  computed: {
-    ...mapGetters(['cartItems']),
-    cartItemsArray() {
-      return this.cartItems;
-    }
-  },
-  methods: {
-    ...mapActions(['removeFromCart']),
-    handleRemove(productId: number) {
-      this.removeFromCart(productId);
-    }
-  }
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+import type { ProductCart } from '../types/ProductCart';
 
-})
+// Accept isActive as a prop
+const props = defineProps<{ isActive: boolean }>();
+
+const store = useStore();
+// Ensure cartItemsArray is reactive
+const cartItemsArray = computed(() => store.getters.cartItems as ProductCart[]);
+
+const handleRemove = (productId: number) => {
+  store.dispatch('removeFromCart', productId);
+}
+
+// import { defineComponent } from 'vue'
+// import { RouterLink } from 'vue-router';
+// import { mapGetters, mapActions } from 'vuex';
+// export default defineComponent({
+//   props: {
+//     isActive: {
+//       type: Boolean,
+//       required: true
+//     }
+//   },
+//   components: {
+//     RouterLink
+//   },
+//   computed: {
+//     ...mapGetters(['cartItems']),
+//     cartItemsArray() {
+//       return this.cartItems;
+//     }
+//   },
+//   methods: {
+//     ...mapActions(['removeFromCart']),
+//     handleRemove(productId: number) {
+//       this.removeFromCart(productId);
+//     }
+//   }
+
+// })
 </script>
 
 <style scoped>
