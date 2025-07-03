360cb145dfec4b3e2c4b7b63f03fd28d7de6d579
Ayat166 <ayatali1661@gmail.com>
2025-07-03 19:19:47 +0300

    migrate from vuex to pinia in stors

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,6 +1,6 @@
-import { createStore } from 'vuex'
 import type { Product } from '../types/Product'
 import type { ProductCart } from '../types/ProductCart'
+import { defineStore } from 'pinia'
 
 export interface State {
   products: Product[]
@@ -11,56 +11,53 @@ export interface Commit {
   (type: string, payload?: unknown): void
 }
 
-export default createStore({
-  state: {
+export const useStore = defineStore('store', {
+  state: () => ({
     products: [] as Product[],
     product: null,
     cart: [] as ProductCart[],
-  },
-  mutations: {
-    setProducts(state: State, products: Product[]) {
-      state.products = products
-    },
-    setProduct(state: State, product: Product | null) {
-      state.product = product
-    },
-    addCartItem(state: State, item: ProductCart) {
-      state.cart.push(item)
-    },
-    updateCartItemQuantity(state: State, payload: { productId: number; quantity: number }) {
-      const existingProduct = state.cart.find((item) => item.product.id === payload.productId)
-      if (existingProduct) {
-        existingProduct.quantity = payload.quantity
-      }
-    },
-    removeCartItem(state: State, productId: number) {
-      state.cart = state.cart.filter((item) => item.product.id !== productId)
-    },
-  },
+  }),
+  // mutations: {
+  //   setProducts(state: State, products: Product[]) {
+  //     state.products = products
+  //   },
+  //   setProduct(state: State, product: Product | null) {
+  //     state.product = product
+  //   },
+  //   addCartItem(state: State, item: ProductCart) {
+  //     state.cart.push(item)
+  //   },
+  //   updateCartItemQuantity(state: State, payload: { productId: number; quantity: number }) {
+  //     const existingProduct = state.cart.find((item) => item.product.id === payload.productId)
+  //     if (existingProduct) {
+  //       existingProduct.quantity = payload.quantity
+  //     }
+  //   },
+  //   removeCartItem(state: State, productId: number) {
+  //     state.cart = state.cart.filter((item) => item.product.id !== productId)
+  //   },
+  // },
   actions: {
-    async fetchProducts({ commit }: { commit: Commit }) {
+    async fetchProducts() {
       const res = await fetch('https://fakestoreapi.com/products')
       const data = await res.json()
-      commit('setProducts', data)
+      this.products = data
     },
-    async fetchProduct({ commit }: { commit: Commit }, id: number) {
+    async fetchProduct(id: number) {
       const res = await fetch(`https://fakestoreapi.com/products/${id}`)
       const data = await res.json()
-      commit('setProduct', data)
+      this.product = data
     },
-    addToCart({ state, commit }: { state: State; commit: Commit }, product: Product) {
-      const existingProduct = state.cart.find((item) => item.product.id === product.id)
+    addToCart(product: Product) {
+      const existingProduct = this.cart.find((item) => item.product.id === product.id)
       if (existingProduct) {
-        commit('updateCartItemQuantity', {
-          productId: product.id,
-          quantity: existingProduct.quantity + 1,
-        })
+        existingProduct.quantity += 1
       } else {
-        commit('addCartItem', { product, quantity: 1 })
+        this.cart.push({ product, quantity: 1 })
       }
     },
-    removeFromCart({ commit }: { commit: Commit }, productId: number) {
-      commit('removeCartItem', productId)
+    removeFromCart(productId: number) {
+      this.cart = this.cart.filter((item) => item.product.id !== productId)
     },
   },
   getters: {
